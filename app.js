863bf7558f1fb0d13baad803d7fa30553c5876a7
Yeoh Joer <yeoh.joer@outlook.com>
2021-01-26 05:58:11 +0800

    Move user interface related code to a new file

---
+++ app.js
@@ -0,0 +1,103 @@
+//Initialize the image for texture mapping
+var cubeImage = document.getElementById("cubeImage1");
+configureCubeTexture(cubeImage);
+
+var sphereImage = document.getElementById("sphereImage1");
+configureSphereTexture(sphereImage);
+
+//Image for texture mapping
+document.getElementById("Image1").onclick = function () {
+  cubeImage = document.getElementById("cubeImage1");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage1");
+  configureSphereTexture(sphereImage);
+};
+
+document.getElementById("Image2").onclick = function () {
+  cubeImage = document.getElementById("cubeImage2");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage2");
+  configureSphereTexture(sphereImage);
+};
+
+document.getElementById("Image3").onclick = function () {
+  cubeImage = document.getElementById("cubeImage3");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage3");
+  configureSphereTexture(sphereImage);
+};
+
+document.getElementById("Image4").onclick = function () {
+  cubeImage = document.getElementById("cubeImage4");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage4");
+  configureSphereTexture(sphereImage);
+};
+
+document.getElementById("Image5").onclick = function () {
+  cubeImage = document.getElementById("cubeImage5");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage5");
+  configureSphereTexture(sphereImage);
+};
+
+document.getElementById("Image6").onclick = function () {
+  cubeImage = document.getElementById("cubeImage6");
+  configureCubeTexture(cubeImage);
+
+  sphereImage = document.getElementById("sphereImage6");
+  configureSphereTexture(sphereImage);
+};
+//Function to get the material shininess
+document.getElementById("materialshininess").onchange = function () {
+  let materialShininess = document.getElementById("materialshininess").value;
+  gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
+};
+
+//Function to get the light position
+document.getElementById("lightpositions").onchange = function () {
+  var x = document.getElementById("lightpositions").value;
+  let lightPosition = vec4(x, 1.0, 1.0, 0.0);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "lightPosition"),
+    flatten(lightPosition)
+  );
+};
+
+//Function to get the ambient light
+document.getElementById("ambientLight").onchange = function () {
+  var x = document.getElementById("ambientLight").value;
+  let lightAmbient = vec4(x, x, 0.1, 1.0);
+  let ambientProduct = mult(lightAmbient, materialAmbient);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "ambientProduct"),
+    flatten(ambientProduct)
+  );
+};
+
+//Function to get the diffuse light
+document.getElementById("diffuseLight").onchange = function () {
+  var x = document.getElementById("diffuseLight").value;
+  let lightDiffuse = vec4(x, x, 0.1, 1.0);
+  let diffuseProduct = mult(lightDiffuse, materialDiffuse);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "diffuseProduct"),
+    flatten(diffuseProduct)
+  );
+};
+
+//Function to get the specular light
+document.getElementById("specularLight").onchange = function () {
+  var x = document.getElementById("specularLight").value;
+  let lightSpecular = vec4(x, x, 0.1, 1.0);
+  let specularProduct = mult(lightSpecular, materialSpecular);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "specularProduct"),
+    flatten(specularProduct)
+  );
+};
--- assignment2.js
+++ assignment2.js
@@ -304,103 +304,16 @@ window.onload = function init() {
     gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vTexCoord );
 
-	//Initialize the image for texture mapping
-    var cubeImage = document.getElementById("cubeImage1");
-    configureCubeTexture( cubeImage );
-
-    var sphereImage = document.getElementById("sphereImage1");
-    configureSphereTexture( sphereImage );
-
-	//Image for texture mapping
-	document.getElementById("Image1").onclick = function(){
-		cubeImage = document.getElementById("cubeImage1");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage1");
-		configureSphereTexture( sphereImage );
-	};
-
-	document.getElementById("Image2").onclick = function(){
-		cubeImage = document.getElementById("cubeImage2");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage2");
-		configureSphereTexture( sphereImage );
-	};
-
-	document.getElementById("Image3").onclick = function(){
-		cubeImage = document.getElementById("cubeImage3");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage3");
-		configureSphereTexture( sphereImage );
-	};
-
-	document.getElementById("Image4").onclick = function(){
-		cubeImage = document.getElementById("cubeImage4");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage4");
-		configureSphereTexture( sphereImage );
-	};
-
-	document.getElementById("Image5").onclick = function(){
-		cubeImage = document.getElementById("cubeImage5");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage5");
-		configureSphereTexture( sphereImage );
-	};
-
-	document.getElementById("Image6").onclick = function(){
-		cubeImage = document.getElementById("cubeImage6");
-        configureCubeTexture( cubeImage );
-
-		sphereImage = document.getElementById("sphereImage6");
-		configureSphereTexture( sphereImage );
-	};
-	//Function to get the material shininess
-	document.getElementById("materialshininess").onchange = function() {
-		materialShininess = document.getElementById("materialshininess").value;
-		gl.uniform1f( gl.getUniformLocation(program,"shininess"),materialShininess );
-	};
-
-	//Function to get the light position
-	document.getElementById("lightpositions").onchange = function() {
-		var x = document.getElementById("lightpositions").value;
-		lightPosition = vec4(x, 1.0, 1.0, 0.0 );
-		gl.uniform4fv( gl.getUniformLocation(program,"lightPosition"),flatten(lightPosition) );
-	};
-
-	//Function to get the ambient light
-	document.getElementById("ambientLight").onchange = function() {
-		var x = document.getElementById("ambientLight").value;
-		lightAmbient = vec4(x, x, 0.1, 1.0 );
-		ambientProduct = mult(lightAmbient, materialAmbient);
-		gl.uniform4fv( gl.getUniformLocation(program,"ambientProduct"),flatten(ambientProduct) );
-	};
-
-	//Function to get the diffuse light
-	document.getElementById("diffuseLight").onchange = function() {
-		var x = document.getElementById("diffuseLight").value;
-		lightDiffuse = vec4(x, x, 0.1, 1.0 );
-		diffuseProduct = mult(lightDiffuse, materialDiffuse);
-		gl.uniform4fv( gl.getUniformLocation(program,"diffuseProduct"),flatten(diffuseProduct) );
-	};
-
-	//Function to get the specular light
-	document.getElementById("specularLight").onchange = function() {
-		var x = document.getElementById("specularLight").value;
-		lightSpecular = vec4(x, x, 0.1, 1.0 );
-		specularProduct = mult(lightSpecular, materialSpecular);
-		gl.uniform4fv( gl.getUniformLocation(program,"specularProduct"),flatten(specularProduct) );
-	};
-
     gl.uniform4fv( gl.getUniformLocation(program,"ambientProduct"),flatten(ambientProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,"diffuseProduct"),flatten(diffuseProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,"specularProduct"),flatten(specularProduct) );
     gl.uniform4fv( gl.getUniformLocation(program,"lightPosition"),flatten(lightPosition) );
     gl.uniform1f( gl.getUniformLocation(program,"shininess"),materialShininess );
+
+    // Load procedures dependent on the user interface controls
+    let script = document.createElement("script");
+    script.src = "app.js";
+    document.head.appendChild(script);
 
     render();
 }
