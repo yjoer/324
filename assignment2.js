67b49a8e1d887d7e31c1065ff5dfa4246f7c6fc0
Yeoh Joer <yeoh.joer@outlook.com>
2021-01-26 05:28:00 +0800

    Format code

--- assignment2.js
+++ assignment2.js
@@ -117,14 +117,13 @@ function quad(a, b, c, d) {
   texCoordsArray.push(texCoord[3]);
 }
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+function colorCube() {
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
 }
 
 //Function to create triangle
